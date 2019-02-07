40fe1c1d4e0cb38189d73b780b3d955a0b38d9f0
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-07 22:19:01 +0100

    [TG-22] Add general getters for rectangle interface

--- src/interfaces/models/RectangleInterface.java
+++ src/interfaces/models/RectangleInterface.java
@@ -1,11 +1,18 @@
 package interfaces.models;
 
 public interface RectangleInterface {
-    boolean contains(PointInterface point);
-    boolean intersects(RectangleInterface rectangle);
-
-    Integer distanceTo(PointInterface point);
-    Integer distanceTo(RectangleInterface rectangle);
-    Integer getWidth();
-    Integer getHeight();
+    Boolean contains(PointInterface point);
+    Boolean intersects(RectangleInterface rectangle);
+    Boolean equals(RectangleInterface rectangle);
+    Float getHeight();
+    Float getWidth();
+    Float getXMin();
+    Float getXMax();
+    Float getYMin();
+    Float getYMax();
+    PointInterface getBottomLeft();
+    PointInterface getTopLeft();
+    PointInterface getBottomRight();
+    PointInterface getTopRight();
+    PointInterface getCenter();
 }
