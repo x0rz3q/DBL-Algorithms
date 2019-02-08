1d4c0a32cb4b906c02b1f84113dfd0d605f2ff40
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-08 21:00:16 +0100

    [TG-21] Add javadoc to Point Interface

--- src/interfaces/models/PointInterface.java
+++ src/interfaces/models/PointInterface.java
@@ -1,8 +1,35 @@
 package interfaces.models;
 
 public interface PointInterface {
+    /**
+     * Get the X coordinate.
+     *
+     * @return Float
+     */
     Float getX();
+
+    /**
+     * Get the Y coordinate.
+     *
+     * @return Float
+     */
     Float getY();
+
+    /**
+     * Get the euclidean distance.
+     *
+     * @param p {@link PointInterface}
+     * @return Float
+     * @post {@code \result = sqrt((this.getX() - p.getX())^2 + (this.getY() - p.getY())^2)}
+     */
     Float distance(PointInterface p);
+
+    /**
+     * Equality check.
+     *
+     * @param p {@link PointInterface}
+     * @return Boolean
+     * @post {@code \result = this.getX() == p.getX() && this.getY() == p.getY()}
+     */
     Boolean equals(PointInterface p);
 }
