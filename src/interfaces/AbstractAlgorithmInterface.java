7c82d4e94526b01cb62b399b0530aa6181cf227c
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-08 21:35:25 +0100

    [TG-24] Add javadoc to Abstract Algorithm Interface

--- src/interfaces/AbstractAlgorithmInterface.java
+++ src/interfaces/AbstractAlgorithmInterface.java
@@ -4,8 +4,10 @@ import interfaces.models.RectangleInterface;
 
 public interface AbstractAlgorithmInterface<T extends RectangleInterface> {
     /**
+     * Place all labels with the height being maximized.
+     *
      * @Modifies nodes
-     * @param nodes
+     * @param nodes {@link AbstractAlgorithmInterface}
      */
     void solve(AbstractCollectionInterface<T> nodes);
 }
