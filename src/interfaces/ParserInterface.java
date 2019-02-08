70d4a5e9d4498f81d2cf31d54c976ef1894c9edc
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-08 21:36:38 +0100

    [TG-25] Add javadoc to Parser Interface

--- src/interfaces/ParserInterface.java
+++ src/interfaces/ParserInterface.java
@@ -3,9 +3,26 @@ package interfaces;
 import models.InputRecord;
 import models.OutputRecord;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 public interface ParserInterface {
-    InputRecord input(Readable source);
-    OutputStream output(OutputRecord record);
+    /**
+     * Parse input to program structure.
+     *
+     * @param source {@link Readable}
+     * @return InputRecord
+     * @throws NullPointerException if {@code source == null}
+     * @throws IOException if read error occurs
+     */
+    InputRecord input(Readable source) throws NullPointerException, IOException;
+
+    /**
+     * Serialize internal program structure towards output stream.
+     *
+     * @param record {@link OutputRecord}
+     * @return OutputStream
+     * @throws NullPointerException if {@code record == null}
+     */
+    OutputStream output(OutputRecord record) throws NullPointerException;
 }
