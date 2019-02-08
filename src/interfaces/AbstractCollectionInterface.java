d55445ad6f6bcd2d67ee57f0e8833623507f408f
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-08 21:36:12 +0100

    [TG-23] Add javadoc to Abstract Collection Interface

--- src/interfaces/AbstractCollectionInterface.java
+++ src/interfaces/AbstractCollectionInterface.java
@@ -1,11 +1,42 @@
 package interfaces;
 
+import interfaces.models.RectangleInterface;
+
 import java.util.List;
 
 public interface AbstractCollectionInterface<T> extends Iterable<T> {
-    void insert(T node);
-    void remove(T node);
-    List<T> query2D(T node);
+    /**
+     * Insert node into collection.
+     *
+     * @param node T
+     * @pre {@code node <> null}
+     * @post {@code this.intersects(node)}
+     */
+    void insert(T node) throws NullPointerException;
+
+    /**
+     * Remove node from collection.
+     *
+     * @param node T
+     * @pre {@code node <> null}
+     * @post {@code !this.intersects(node)}
+     */
+    void remove(T node) throws NullPointerException;
+
+    /**
+     * Get all items that intersect with a given range.
+     *
+     * @param range {@link RectangleInterface}
+     * @return List
+     * @post {@code (\forall i; \result.has(i); range.contains(\result.get(i)))}
+     */
+    List<T> query2D(RectangleInterface range);
+
+    /**
+     * Check if an item intersects with given node.
+     * @param node
+     * @return
+     */
     Boolean intersects(T node);
     int size();
 }
