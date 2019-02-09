e7dc0789fed946d7d8b729150f4d5699be5d5a17
JurisMajors <j.a.majors@student.tue.nl>
2019-02-09 15:53:36 +0100

    made Collections package, moved AbstractCollection in there

---
+++ src/Collections/KDTree.java
@@ -0,0 +1,45 @@
+package Collections;
+
+import interfaces.models.GeometryInterface;
+import interfaces.models.RectangleInterface;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class KDTree extends AbstractCollection{
+    @Override
+    public void insert(GeometryInterface node) throws NullPointerException {
+        
+    }
+
+    @Override
+    public void insert(Collection nodes) throws NullPointerException {
+
+    }
+
+    @Override
+    public void remove(GeometryInterface node) throws NullPointerException {
+
+    }
+
+    @Override
+    public Collection query2D(RectangleInterface range) {
+        return null;
+    }
+
+    @Override
+    public Boolean intersects(GeometryInterface node) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+    // not supported yet
+}
---
+++ src/Collections/QuadTree.java
@@ -0,0 +1,8 @@
+package Collections;
+
+import java.util.AbstractCollection;
+import java.util.Iterator;
+
+public class QuadTree
+{
+}
