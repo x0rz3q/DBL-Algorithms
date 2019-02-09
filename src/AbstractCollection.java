92e91f5c0ffe0d728dc2e5e9654f43dbfe667c37
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-09 12:06:31 +0100

    [TG-23] Add abstract basic implementation of Collection

---
+++ src/AbstractCollection.java
@@ -0,0 +1,21 @@
+import interfaces.AbstractCollectionInterface;
+
+import java.util.Collection;
+
+public abstract class AbstractCollection<T> implements AbstractCollectionInterface<T>{
+    protected int count;
+    protected T root;
+
+    public AbstractCollection() {
+        this.count = 0;
+    }
+
+    public AbstractCollection(Collection<T> nodes) {
+        this();
+        this.insert(nodes);
+    }
+
+    public int getSize() {
+        return this.count;
+    }
+}
