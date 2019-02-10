37ae5d262b83d2346c742759cf11383a29269ba3
BenjaminRodatz <b.p.rodatz@student.tue.nl>
2019-02-10 16:59:26 +0100

    started on TwoPositionBinarySearcher

--- src/algorithms/BinarySearcher.java
+++ src/algorithms/BinarySearcher.java
@@ -13,15 +13,23 @@ public abstract class BinarySearcher<T extends RectangleInterface> implements Ab
      */
     @Override
     public void solve(AbstractCollectionInterface<T> nodes) {
+
+        // TODO: needs to be grabbed from input
+        float alpha = 1.4f;
+
         // TODO: this value can be optimized base on the number of points
-        int high = 10000;
+        int high = (int)(10000 * Math.sqrt(alpha));
         int low = 0;
+
+
+        float ratio = 1.4f;
 
 
         // make sure that our upper bound is correct. This may not be required if we have a good estimation
         while (isSolvable(nodes, high)) {
             low = high;
-            high *= 5;
+            // TODO: value can be optimized (see earlier TODO)
+            high *= 2;
         }
 
 
@@ -37,10 +45,20 @@ public abstract class BinarySearcher<T extends RectangleInterface> implements Ab
         float height = low;
 
 
+        // TODO: this needs to be fixed
+        System.out.println(low + " " + high);
 
+        float newHeight = (float) Math.floor(high*ratio) / ratio;
+        if (newHeight > height && isSolvable(nodes, newHeight)) {
+            height = newHeight;
+        }
+
+        newHeight = (float) (Math.floor(high*ratio) - 0.5) / ratio;
+        if (newHeight > height && isSolvable(nodes, newHeight)) {
+            height = newHeight;
+        }
 
         getSolution(nodes, height);
-        System.out.println(low + " " + high);
     }
 
     /**
@@ -53,11 +71,12 @@ public abstract class BinarySearcher<T extends RectangleInterface> implements Ab
     abstract boolean isSolvable(AbstractCollectionInterface<T> nodes, float height);
 
     /**
-     * Place all labels with the given height
+     * Place all labels with the given height (this method is not robust to reduce computation time)
      *
      * @modifies nodes
      * @param nodes {@link AbstractAlgorithmInterface}
      * @param height required height for rectangles
+     * @pre isSolvable(nodes, height)
      */
     abstract void getSolution(AbstractCollectionInterface<T> nodes, float height);
 
--- src/algorithms/TwoPositionBinarySearcher.java
+++ src/algorithms/TwoPositionBinarySearcher.java
@@ -3,15 +3,164 @@ package algorithms;
 import interfaces.AbstractCollectionInterface;
 import interfaces.models.RectangleInterface;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class TwoPositionBinarySearcher<T extends RectangleInterface> extends BinarySearcher<T> {
+
+
+    // variable for dfs to keep track of visited nodes
+    private boolean[] visited;
+    private Stack<Integer> s;
+
+    // edges of implication graph and its inverse
+    private ArrayList<Integer>[] adj;
+    private ArrayList<Integer>[] adjInv;
+
+
+    // stores for each node the component it is in
+    private int[] scc;
+
+    // keep track of current component
+    private int counter;
+
+
+    // keeps track of the rect height of last created graph
+    private float heightLastGraph;
+
+
+    // keeps track of which rectangles have been set in getSolution()
+    private boolean[] isSet;
+
+
     @Override
     boolean isSolvable(AbstractCollectionInterface<T> nodes, float height) {
-        System.out.println("TwoPositionBinarySearcher.isSolvable() height: " + height);
-        return height <= 500;
+        if (heightLastGraph != height) {
+            createGraph(nodes, height);
+        }
+        return Kosaraju(nodes.size());
+
     }
 
     @Override
     void getSolution(AbstractCollectionInterface<T> nodes, float height) {
+        if (heightLastGraph != height) {
+            createGraph(nodes, height);
+        }
 
+        isSet = new boolean[nodes.size()];
+
+        for (int i = 0; i < nodes.size(); i++) {
+            if (!isSet[i]) {
+                assignTrue(i, nodes.size());
+            }
+        }
+    }
+
+
+    private void createGraph(AbstractCollectionInterface<T> nodes, float height) {
+
+        heightLastGraph = height;
+
+        // ------------ initialize variables ------------
+        visited = new boolean[nodes.size() * 2];
+        s = new Stack<>();
+        scc = new int[nodes.size() * 2];
+        counter = 0;
+
+        adj = new ArrayList[nodes.size() * 2];
+        adjInv = new ArrayList[nodes.size() * 2];
+
+
+        int noInputs = nodes.size();
+
+        for (int i = 0; i < noInputs * 2; i++) {
+            adj[i] = new ArrayList<>();
+            adjInv[i] = new ArrayList<>();
+        }
+
+
+        // ------------ adding edges to graph ------------
+        // TODO: I need the right data structure for that
+        for (T n : nodes) {
+            nodes.query2D(n);
+        }
+    }
+
+    private boolean Kosaraju(int noInputs) {
+        // Step 1: dfs on original graph
+        for (int i = 0; i < noInputs * 2; i++) {
+            if (!visited[i]) {
+                dfsFirst(i);
+            }
+        }
+
+        // Step 2: traverse inverse graph based on s
+        while (!s.empty()) {
+            int n = s.pop();
+
+            if (visited[n]) {
+                dfsSecond(n);
+                counter++;
+
+            }
+        }
+
+        for (int i = 0; i < noInputs; i++) {
+            if (scc[i] == scc[i + noInputs]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private void addEdgeAndInverse(int a, int b) {
+        adj[a].add(b);
+        adjInv[b].add(a);
+    }
+
+
+    private void dfsFirst(int start) {
+        if (visited[start]) {
+            return;
+        }
+
+        visited[start] = true;
+
+        for (int i : adj[start]) {
+            dfsFirst(i);
+        }
+
+        s.push(start);
+    }
+
+    private void dfsSecond(int start) {
+        if (!visited[start]) {
+            return;
+        }
+        visited[start] = false;
+
+        for (int i : adjInv[start]) {
+            dfsSecond(i);
+        }
+
+        scc[start] = counter;
+    }
+
+    private void assignTrue(int start, int noNodes) {
+
+
+        if (start < noNodes) {
+            isSet[start] = true;
+            // TODO: set start to true value(needs data structures)
+        } else {
+            isSet[start - noNodes] = true;
+            // TODO: set start % noNodes to false value(needs data structures)
+        }
+
+        for (int i : adj[start]) {
+            assignTrue(i, noNodes);
+        }
     }
 }
