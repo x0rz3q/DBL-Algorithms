964f419883f0a4b7aaf150354986e27c16700633
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-11 21:10:37 +0100

    Let collections extend square interface

--- src/AbstractCollection.java
+++ src/AbstractCollection.java
@@ -1,8 +1,9 @@
 import interfaces.AbstractCollectionInterface;
+import interfaces.models.SquareInterface;
 
 import java.util.Collection;
 
-public abstract class AbstractCollection<T> implements AbstractCollectionInterface<T>{
+public abstract class AbstractCollection<T extends SquareInterface> implements AbstractCollectionInterface {
     protected int count;
     protected T root;
 
