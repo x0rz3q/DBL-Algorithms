b851793da4fa0de4ea71d9200773fe5ed20cf0db
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2019-02-13 15:01:31 +0100

    Created TestFiles for quadTree

---
+++ tests/Collections/AbstractCollectionTest.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractCollectionTest {
+
+    @BeforeEach
+    void setup() { }
+
+
+
+    @Test
+    void getSize() {
+    }
+}
---
+++ tests/Collections/QuadTreeTest.java
@@ -0,0 +1,32 @@
+package Collections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuadTreeTest {
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void insert1() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void query2D() {
+    }
+
+    @Test
+    void intersects() {
+    }
+
+    @Test
+    void size() {
+    }
+}
