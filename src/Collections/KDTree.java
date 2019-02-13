dd465fc88d7f1348c2e540cc2c3ab60887e994af
JurisMajors <j.a.majors@student.tue.nl>
2019-02-13 22:42:51 +0100

    Skeleton code for KDTree with a constructor.

--- src/Collections/KDTree.java
+++ src/Collections/KDTree.java
@@ -6,6 +6,37 @@ import models.BoundingBox;
 import java.util.Collection;
 
 public class KDTree extends AbstractCollection{
+    Collection<SquareInterface> nodes;
+    boolean isLeaf; /** does it directly contain the nodes **/
+    int depth;
+    /** children of this tree **/
+    KDTree left;
+    KDTree right;
+
+    /**
+     * Constructor only for the client, initializing the root
+     * @param nodes Collection of {@link SquareInterface} to be contained in the tree
+     */
+    public KDTree(Collection<SquareInterface> nodes) {
+        this(nodes, 0);
+    }
+
+    KDTree(Collection<SquareInterface> nodes, int depth) {
+        this.nodes = nodes;
+        this.depth = depth;
+        isLeaf = false;
+        setDataLimit(1);
+        buildTree(this.nodes, this.depth);
+    }
+
+    /**
+     * Builds a subtree
+     * @param nodes what nodes to put in this subtree
+     * @param depth depth of the tree
+     * @return
+     */
+    private KDTree buildTree(Collection<SquareInterface> nodes, int depth) {return null;}
+
     @Override
     public void insert(SquareInterface node) throws NullPointerException {
 
