83bb7a0b657ed3e107d0b9e5e4f4fb4bbf26dc7f
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2019-02-13 15:39:54 +0100

    Implemented some testcases Quadtree
    Implemented 2 size testcases and 1 insert test case

--- tests/Collections/QuadTreeTest.java
+++ tests/Collections/QuadTreeTest.java
@@ -2,8 +2,14 @@ package Collections;
 
 import interfaces.models.SquareInterface;
 import models.Anchor;
+import models.Point;
 import models.Square;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class QuadTreeTest extends AbstractCollectionTest {
 
@@ -16,15 +22,19 @@ class QuadTreeTest extends AbstractCollectionTest {
 
     @Test
     void insert() {
+        SquareInterface point1 = new Point(5000, 5000);
+        this.instance.insert(point1);
+        SquareInterface range = new Square(new Anchor(4000, 4000), 2000);
+        Collection<SquareInterface> points = this.instance.query2D(range);
+        assertTrue(points.contains(point1));
     }
 
-    @Test
-    void insert1() {
-    }
-
+    /**
+     * Not implemented yet
     @Test
     void remove() {
     }
+     */
 
     @Test
     void query2D() {
@@ -36,5 +46,17 @@ class QuadTreeTest extends AbstractCollectionTest {
 
     @Test
     void size() {
+        assertEquals(0, this.instance.getSize());
+    }
+
+    @Test
+    void size2() {
+        SquareInterface point1 = new Point(10, 50);
+        SquareInterface point2 = new Point(500, 500);
+        SquareInterface point3 = new Point(2000, 3000);
+        this.instance.insert(point1);
+        this.instance.insert(point2);
+        this.instance.insert(point3);
+        assertEquals(3, this.instance.getSize());
     }
 }
