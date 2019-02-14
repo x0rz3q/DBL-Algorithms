17618a6e684de61375d41898de215dbb114779cd
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-02-14 21:43:53 +0100

    Add controller and main wrapper to run via file argument

---
+++ src/Controller.java
@@ -0,0 +1,27 @@
+import Collections.QuadTree;
+import interfaces.ParserInterface;
+import Parser.*;
+
+import java.io.IOException;
+
+public class Controller {
+    private ParserInterface parser;
+
+    public Controller() {
+        this.parser = new Parser();
+    }
+
+    public void run() throws IOException, NullPointerException{
+        DataRecord record = this.parser.input(System.in, QuadTree.class);
+
+        this.parser.output(record, System.out);
+    }
+
+    public static void main(String[] args) {
+        try {
+            (new Controller()).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
