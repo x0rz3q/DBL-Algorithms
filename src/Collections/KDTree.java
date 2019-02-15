a2640e74070d3efd82c8a2f55847fc94556c4ad9
JurisMajors <j.a.majors@student.tue.nl>
2019-02-15 12:32:13 +0100

    Seperate constructors for leaves and roots. buildTree skeleton

--- src/Collections/KDTree.java
+++ src/Collections/KDTree.java
@@ -12,30 +12,51 @@ public class KDTree extends AbstractCollection{
     /** children of this tree **/
     KDTree left;
     KDTree right;
+    SquareInterface splitter;
 
     /**
-     * Constructor only for the client, initializing the root
+     * Constructor for initializing a non-leaf KDTree (e.g. root).
+     * used by client and builder.
      * @param nodes Collection of {@link SquareInterface} to be contained in the tree
+     * @param depth Depth of this root.
      */
-    public KDTree(Collection<SquareInterface> nodes) {
-        this(nodes, 0);
+    public KDTree(Collection<SquareInterface> nodes, int depth) {
+        isLeaf = false;
+        this.depth = depth;
+        buildTree(nodes, depth);
     }
 
-    KDTree(Collection<SquareInterface> nodes, int depth) {
-        this.nodes = nodes;
+    /**
+     * Constructor for initializing a leaf.
+     * @param nodes contained in the leaf
+     * @param depth depth of the leaf
+     * @param limit limit of data of when to split down further
+     */
+    KDTree(Collection<SquareInterface> nodes, int depth, int limit) {
         this.depth = depth;
-        isLeaf = false;
-        setDataLimit(1);
-        buildTree(this.nodes, this.depth);
+        this.nodes = nodes;
+        this.isLeaf = true;
+        setDataLimit(limit);
     }
 
     /**
      * Builds a subtree
      * @param nodes what nodes to put in this subtree
-     * @param depth depth of the tree
+     * @param depth depth of the root
      * @return
      */
-    private KDTree buildTree(Collection<SquareInterface> nodes, int depth) {return null;}
+    private KDTree buildTree(Collection<SquareInterface> nodes, int depth) {
+        if (nodes.size() <= this.dataLimit) { // if below data limit
+            return new KDTree(nodes, depth, this.dataLimit); // construct a leaf
+        } else {
+            if (depth % 2 == 0) { // even depth
+                // split vertically
+            } else { // odd depth
+                // split horizontally
+            }
+        }
+        return null;
+    }
 
     @Override
     public void insert(SquareInterface node) throws NullPointerException {
