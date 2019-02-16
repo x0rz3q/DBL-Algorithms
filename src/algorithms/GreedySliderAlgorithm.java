4183fcbe6abac47327ff40e501bc3fd02caf9790
Chess <34745596+Chessnl@users.noreply.github.com>
2019-02-16 14:59:07 +0100

    [TG-53] Wrote initial structure for the GreedySliderAlgorithm
    Added a class implementing AbstractAlgorithmInterface with javadoc for unimplemented functions.
    
    Main task, get this structure checked and decide correct data-types for the DataRecord.collection.

---
+++ src/algorithms/GreedySliderAlgorithm.java
@@ -0,0 +1,69 @@
+package algorithms;
+
+/*
+ * @author = Jeroen Schols
+ */
+
+import Parser.DataRecord;
+import interfaces.AbstractAlgorithmInterface;
+import interfaces.models.SquareInterface;
+import models.Point;
+
+import java.util.List;
+
+public class GreedySliderAlgorithm implements AbstractAlgorithmInterface {
+
+    private DataRecord record;
+
+    @Override
+    public void solve(DataRecord record) {
+        this.record = record;
+        Point[] sortedPoints = sortPoints(record.points);
+    }
+
+    /**
+     * sorts a set of points into an array such that a point p is before point q if {@code p.x < q.x || (p.x == q.x && p.y > q.y)}
+     *
+     * @param points {A List of points}
+     * @return An array of all points sorted on increasing x-coordinates and on decreasing y-coordinates
+     */
+    private Point[] sortPoints(List<? extends SquareInterface> points) {
+        throw new UnsupportedOperationException("GreedySliderAlgorithm.sortPoints() not implemented yet");
+    }
+
+    /**
+     * returns whether it is possible to label each point with a label of given width
+     *
+     * @param points {A list of points}
+     * @param width float denoting the width given to each label
+     * @modifies record.collection
+     * @return whether there is a valid possible labeling
+     */
+    private boolean isSolvable(Point[] points, float width) {
+        throw new UnsupportedOperationException("GreedySliderAlgorithm.isSolvable() not implemented yet");
+    }
+
+    /**
+     * labels each point with a label of given width
+     *
+     * @param points {A list of points}
+     * @param width float denoting the width given to each label
+     * @modifies record.collection
+     * @return whether there is a valid possible labeling
+     */
+    private boolean solve(Point[] points, float width) {
+        throw new UnsupportedOperationException("GreedySliderAlgorithm.solve() not implemented yet");
+    }
+
+    /**
+     * makes a greedy choice for assigning given point a label of given width by placing it with a smalles possible slider value
+     *
+     * @param point {@link Point}
+     * @param width float denoting the width given to this label
+     * @modifies record.collection
+     * @return whether it is possible to add a label of given width to the collection
+     */
+    private boolean addLabel(Point point, float width) {
+        throw new UnsupportedOperationException("GreedySliderAlgorithm.setLabel() not implemented yet");
+    }
+}
