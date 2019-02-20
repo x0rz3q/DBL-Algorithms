f903ed9b264b246cb1cd0517ca994d447400a910
BenjaminRodatz <b.p.rodatz@student.tue.nl>
2019-02-20 16:11:35 +0100

    slightly refactored algorithmTester

--- tests/algorithms/AlgorithmTester.java
+++ tests/algorithms/AlgorithmTester.java
@@ -3,6 +3,7 @@ package algorithms;
 import Collections.QuadTree;
 import Parser.DataRecord;
 import Parser.Parser;
+import interfaces.AbstractAlgorithmInterface;
 import javafx.util.Pair;
 import main.Interpreter;
 import org.junit.jupiter.api.Test;
@@ -15,18 +16,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AlgorithmTester {
 
-    private void runTest(DataRecord record, String fileName, Double optHeight) {
+    private void runTest(DataRecord record, String fileName, Double optHeight, AbstractAlgorithmInterface algorithms) {
 
-        switch (record.placementModel) {
-            case ONE_SLIDER:
-                (new GreedySliderAlgorithm()).solve(record);
-                break;
-            case TWO_POS:
-                (new TwoPositionBinarySearcher()).solve(record);
-                break;
-            case FOUR_POS:
-                throw new UnsupportedOperationException("No 4-pos algorithm implemented yet ");
-        }
+        algorithms.solve(record);
 
         assertEquals(optHeight * 1.0, (record.height / record.aspectRatio), "the height found is not correct in file: " + fileName);
         assertTrue(Interpreter.isValid(record), "the solution found is not valid in file: " + fileName);
@@ -34,7 +26,7 @@ public class AlgorithmTester {
     }
 
 
-    private void readInFiles(String filePath) throws IOException, NullPointerException {
+    private void readInFiles(String filePath, AbstractAlgorithmInterface algorithm) throws IOException, NullPointerException {
         File folder = new File(filePath);
         File[] listOfFiles = folder.listFiles();
 
@@ -43,7 +35,7 @@ public class AlgorithmTester {
         for (int i = 0; i < listOfFiles.length; i++) {
             if (listOfFiles[i].isFile()) {
                 Pair<DataRecord, Double> input = parser.inputTestMode(new FileInputStream(listOfFiles[i]), QuadTree.class);
-                runTest(input.getKey(), listOfFiles[i].getName(), input.getValue());
+                runTest(input.getKey(), listOfFiles[i].getName(), input.getValue(), algorithm);
             }
         }
     }
@@ -52,7 +44,7 @@ public class AlgorithmTester {
     @Test
     public void TwoPosTest() {
         try {
-            readInFiles("tests/algorithms/TestFiles/TwoPosTestFiles");
+            readInFiles("tests/algorithms/TestFiles/TwoPosTestFiles", new TwoPositionBinarySearcher());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -60,7 +52,7 @@ public class AlgorithmTester {
     @Test
     public void FourPosTest() {
         try {
-            readInFiles("tests/algorithms/TestFiles/FourPosTestFiles");
+            readInFiles("tests/algorithms/TestFiles/FourPosTestFiles", null);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -68,7 +60,7 @@ public class AlgorithmTester {
     @Test
     public void SliderTest() {
         try {
-            readInFiles("tests/algorithms/TestFiles/SliderTestFiles");
+            readInFiles("tests/algorithms/TestFiles/SliderTestFiles", new GreedySliderAlgorithm());
         } catch (IOException e) {
             e.printStackTrace();
         }
