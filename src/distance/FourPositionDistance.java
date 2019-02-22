e7befdfce7cb75829bad91fc3c04c43c4ffdc5da
JurisMajors <j.a.majors@student.tue.nl>
2019-02-22 11:41:01 +0100

    added calculate method to Distance Abstraction

--- src/distance/FourPositionDistance.java
+++ src/distance/FourPositionDistance.java
@@ -1,4 +1,10 @@
 package distance;
 
+import interfaces.models.PointInterface;
+
 public class FourPositionDistance extends AbstractDistance {
+    @Override
+    public double calculate(PointInterface p1, PointInterface p2) {
+        return 0;
+    }
 }
