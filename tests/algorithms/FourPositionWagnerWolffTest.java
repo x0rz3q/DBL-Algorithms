8ce3b02e994f28b7f0e0382bed65da948fdeb224
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2019-02-22 14:29:24 +0100

    Added TestGeneration class for 4pos

---
+++ tests/algorithms/FourPositionWagnerWolffTest.java
@@ -0,0 +1,40 @@
+package algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FourPositionWagnerWolffTest {
+
+    @Test
+    void findConflictSizes() {
+    }
+
+    @Test
+    void preprocessing() {
+    }
+
+    @Test
+    void createConflictGraph() {
+    }
+
+    @Test
+    void eliminateImpossibleCandidates() {
+    }
+
+    @Test
+    void applyHeuristic() {
+    }
+
+    @Test
+    void doTwoSat() {
+    }
+
+    @Test
+    void isSolvable() {
+    }
+
+    @Test
+    void getSolution() {
+    }
+}
