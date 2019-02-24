b081d8034da4810b8bb2af7ee9581450d39fbb3f
Juris Arturs Majors <32071671+JurisMajors@users.noreply.github.com>
2019-02-24 11:12:01 +0100

    Insert fix and Tested nearest neighbours (#39)
    * fixed insert and untested nearest neighbours
    
    * added distances package
    
    * Update src/distance/FourPositionDistance.java
    
    Co-Authored-By: JurisMajors <32071671+JurisMajors@users.noreply.github.com>
    
    * removed duplicate code
    
    * refactoring, functional decomposition, dealing with edge case, tests
    added
    
    * more reformatting
    
    * use of the inLeft()
    
    * declaration of some variables private
    
    * final test, passed

---
+++ src/distance/AbstractDistance.java
@@ -0,0 +1,7 @@
+package distance;
+
+import interfaces.models.PointInterface;
+
+public abstract class AbstractDistance {
+    public abstract double calculate(PointInterface p1, PointInterface p2);
+}
