e0d31a0adbd6e140ceccd796c5406fd88554aec4
Chess <34745596+Chessnl@users.noreply.github.com>
2019-02-27 15:07:12 +0100

    [TG-72] Updated GreedySliderAlgorithm accordingly

--- src/algorithms/GreedySliderAlgorithm.java
+++ src/algorithms/GreedySliderAlgorithm.java
@@ -8,6 +8,7 @@ import Parser.DataRecord;
 import interfaces.AbstractAlgorithmInterface;
 import interfaces.models.GeometryInterface;
 import interfaces.models.LabelInterface;
+import models.FieldExtendedSliderLabel;
 import models.Point;
 import models.Rectangle;
 import models.SliderLabel;
@@ -41,17 +42,17 @@ public class GreedySliderAlgorithm implements AbstractAlgorithmInterface {
     public void solve(DataRecord record) {
 
         // casts labels to sliderlabels in an new sorted List
-        List<SliderLabel> sortedLabels = new ArrayList<>();
+        List<FieldExtendedSliderLabel> sortedLabels = new ArrayList<>();
         for (LabelInterface label : record.labels) {
-            if (label.getClass() != SliderLabel.class)
+            if (label.getClass() != FieldExtendedSliderLabel.class)
                 throw new IllegalArgumentException("GreedySliderAlgorithm.solve() input record does not provide SliderLabels");
-            else sortedLabels.add((SliderLabel) label);
+            else sortedLabels.add((FieldExtendedSliderLabel) label);
         }
         sortedLabels.sort(comparator);
 
-        double epsilon = 0.0001;
+        double epsilon = 0.000000000000001;
         double low = 0;
-        double high = 1000000000000d;
+        double high = Double.MAX_VALUE;
         while (true) {
             double mid = (low + high) / 2;
             System.out.println("\nmid = " + mid);
@@ -84,11 +85,11 @@ public class GreedySliderAlgorithm implements AbstractAlgorithmInterface {
      * @modifies record
      * @post if there is a solution record.collection contains it, else record.collection holds an invalid solution
      */
-    private boolean solve(DataRecord record, List<SliderLabel> sortedLabels, double width) {
-        for (SliderLabel label : sortedLabels) {
+    private boolean solve(DataRecord record, List<FieldExtendedSliderLabel> sortedLabels, double width) {
+        for (FieldExtendedSliderLabel label : sortedLabels) {
             if (!setLabel(record, label, width)) {
                 for (SliderLabel l : sortedLabels) {
-                    l.setWidth(0);
+                    l.setHeight(0);
                 }
                 return false;
             }
@@ -104,7 +105,7 @@ public class GreedySliderAlgorithm implements AbstractAlgorithmInterface {
      * @param width  double denoting the width assigned to label
      * @return whether it is possible to have label be placeable in collection with given width
      */
-    private boolean setLabel(DataRecord record, SliderLabel label, double width) {
+    private boolean setLabel(DataRecord record, FieldExtendedSliderLabel label, double width) {
         Rectangle queryArea = new Rectangle(
                 label.getPOI().getX() - width,
                 label.getPOI().getY(),
@@ -113,18 +114,21 @@ public class GreedySliderAlgorithm implements AbstractAlgorithmInterface {
         Collection<GeometryInterface> queryResult = record.collection.query2D(queryArea);
 
         double xMax = label.getPOI().getX() - width;
+        FieldExtendedSliderLabel maxLabel = label;
         for (GeometryInterface entry : queryResult) {
-            if (entry != label) xMax = Math.max(xMax, entry.getXMax());
+            if (entry != label) {
+                maxLabel = (FieldExtendedSliderLabel) entry;
+                xMax = Math.max(xMax, entry.getXMax());
+            }
         }
 
         if (xMax > label.getPOI().getX()) return false;
 
-        label.setRectangle(new Rectangle(
-                xMax,
-                label.getPOI().getY(),
-                xMax + width,
-                label.getPOI().getY() + width / record.aspectRatio
-        ));
+        if (xMax == label.getPOI().getX() - width) {
+            label.setFieldExtended((int) label.getPOI().getX(), 0, width);
+        } else {
+            label.setFieldExtended(maxLabel.a, maxLabel.b + 1, width);
+        }
 
         return true;
     }
