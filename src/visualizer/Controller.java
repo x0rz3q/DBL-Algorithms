d42bf8bd09b4e914726ea3f0d745de3f2cdc3aac
xoryo <me@xoryo.nl>
2019-03-01 11:10:25 +0100

    Add new visualizer implementation

--- src/visualizer/Controller.java
+++ src/visualizer/Controller.java
@@ -1,6 +1,7 @@
 package visualizer;
 
 
+import interfaces.models.LabelInterface;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.layout.*;
@@ -8,7 +9,9 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import models.Rectangle;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Controller {
     @FXML
@@ -17,35 +20,36 @@ public class Controller {
 
     public void initialize() {
         mainPanel.setBackground(new Background(new BackgroundFill(Color.web("#000"), CornerRadii.EMPTY, Insets.EMPTY)));
-        this.record = Record.parse();
+        this.record = new Record(System.in);
+//        this.record = Record.parse();
     }
 
     public void setTitle(Stage stage) {
-        stage.setTitle("Placed " + this.record.actualPointCount + "/" + this.record.pointCount + " intersected " + this.record.intersections);
+        stage.setTitle("Placed " + this.record.getLabels().size() + "/" + this.record.getPointCount());
     }
 
     public void redraw() {
         this.mainPanel.getChildren().clear();
-        double scaleFactor = (Math.min(this.mainPanel.getHeight(), this.mainPanel.getWidth())) / this.record.extent;
+        double scaleFactor = (Math.min(this.mainPanel.getHeight(), this.mainPanel.getWidth())) / this.record.getExtent();
 
-        for (Map.Entry<Rectangle, Boolean> entry : this.record.rectangles.entrySet()) {
-            Rectangle rectangle = entry.getKey();
+        this.record.getLabels().forEach((key, value) -> {
+            Rectangle rectangle = key.getRectangle();
 
             javafx.scene.shape.Rectangle shape = new javafx.scene.shape.Rectangle();
-            shape.setLayoutY(this.mainPanel.getHeight() - (rectangle.getYMin() + Math.abs(this.record.shiftY) + rectangle.getHeight()) * scaleFactor);
+            shape.setLayoutY(this.mainPanel.getHeight() - (rectangle.getYMin() + Math.abs(this.record.getyMin()) + rectangle.getHeight()) * scaleFactor);
             shape.setLayoutX(rectangle.getXMin() * scaleFactor);
             shape.setWidth(rectangle.getWidth() * scaleFactor);
             shape.setHeight(rectangle.getHeight() * scaleFactor);
 
-            if(entry.getValue()) {
-                shape.setStroke(Color.RED);
-            } else {
+            if (value.size() == 0) {
                 shape.setStroke(Color.WHITE);
+            } else {
+                shape.setStroke(Color.RED);
             }
 
             shape.setFill(Color.TRANSPARENT);
 
             this.mainPanel.getChildren().add(shape);
-        }
+        });
     }
 }
--- src/visualizer/Record.java
+++ src/visualizer/Record.java
@@ -2,90 +2,96 @@ package visualizer;
 
 import Collections.QuadTree;
 import com.sun.org.apache.regexp.internal.RE;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import interfaces.models.GeometryInterface;
-import models.DirectionEnum;
-import models.PositionLabel;
-import models.Rectangle;
+import interfaces.models.LabelInterface;
+import models.*;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Record {
-    public HashMap<Rectangle, Boolean> rectangles;
-    public int pointCount;
-    public int actualPointCount = 0;
-    public double extent;
-    public int intersections = 0;
-    public double shiftX = 0;
-    public double shiftY = 0;
+    private HashMap<LabelInterface, Set<LabelInterface>> labels;
+    private QuadTree tree;
+    private int pointCount = 0;
+    private double extent;
+    private double xMin = 0;
+    private double yMin = 0;
 
-    public static Record parse() {
-        Record record  = new Record();
+    private String scan(String regex, Scanner scanner) {
+        while(!scanner.hasNext(regex)) scanner.next();
 
-        Pattern pattern = Pattern.compile("aspect ratio:(\\s|\\t)*(?<ratio>\\d+(.\\d+){0,1})|number of points:(\\s|\\t)*(?<numpoints>\\d+)|height:(\\s|\\t)*(?<height>\\d+(.\\d+){0,1})|(?<coords>\\d+ \\d+) (?<direction>NW|NE)", RE.MATCH_MULTILINE);
-        Scanner scanner = new Scanner(System.in);
+        return scanner.next().split(": ")[1];
+    }
 
-        QuadTree tree = new QuadTree(new Rectangle(0, 0, 10000, 10000));
-        record.rectangles = new HashMap<>();
+    public Record(InputStream stream) {
+        this.labels = new HashMap<>();
+        this.tree = new QuadTree(new Rectangle(0, 0, 10000, 10000));
+        Scanner scanner = new Scanner(stream).useDelimiter(System.lineSeparator());
 
-        double xMin = Integer.MAX_VALUE;
-        double xMax = Integer.MIN_VALUE;
-        double yMin = Integer.MAX_VALUE;
-        double yMax = Integer.MIN_VALUE;
-        double aspectRatio = 0.0;
-        double height = 0.0;
+        PlacementModelEnum model = PlacementModelEnum.fromString(scan("placement model: .*", scanner));
+        double ratio = Double.parseDouble(scan("aspect ratio: .*", scanner));
+        double height = Double.parseDouble(scan("height: .*", scanner));
+        int points = Integer.parseInt(scan("number of points: .*", scanner));
 
+        scanner.nextLine();
+        scanner.useDelimiter(Pattern.compile("\\p{javaWhitespace}+"));
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (!line.isEmpty()) {
-                Matcher matcher = pattern.matcher(line);
+        double yMax = 0;
+        double xMax = 0;
 
-                while (matcher.find()) {
-                    if (matcher.group("ratio") != null) {
-                        aspectRatio = Double.parseDouble(matcher.group("ratio"));
-                    } else if(matcher.group("height") != null) {
-                        height = Double.parseDouble(matcher.group("height"));
-                    } else if(matcher.group("numpoints") != null) {
-                        record.pointCount = Integer.parseInt(matcher.group("numpoints"));
-                    } else if (matcher.group("coords") != null) {
-                        record.actualPointCount++;
-                        String[] split = matcher.group("coords").split(" ");
+        while (scanner.hasNext()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
 
-                        PositionLabel label = new PositionLabel(Double.parseDouble(split[0]), Double.parseDouble(split[1]),
-                                height, aspectRatio, -1, DirectionEnum.fromString(matcher.group("direction")));
+            LabelInterface label = null;
 
-                        xMin = Math.min(label.getXMin(), xMin);
-                        xMax = Math.max(label.getXMax(), xMax);
-                        yMin = Math.min(label.getYMin(), yMin);
-                        yMax = Math.max(label.getYMax(), yMax);
+            if (scanner.hasNextDouble()) {
+                double alpha = scanner.nextDouble();
+                label = new SliderLabel(x, y, height,ratio, alpha, 0);
+            } else
+                label = new PositionLabel(x, y, height, ratio, 0, DirectionEnum.fromString(scanner.next()));
 
-                        record.shiftY = Math.min(record.shiftY, label.getRectangle().getYMin());
-                        record.shiftX = Math.min(record.shiftX, label.getRectangle().getXMin());
+            Collection<GeometryInterface> intersections = this.tree.query2D(label.getRectangle());
 
-                        Collection<GeometryInterface> intersection = tree.query2D(label.getRectangle());
-                        record.rectangles.put(label.getRectangle(), intersection.size() > 0);
-                        tree.insert(label.getRectangle());
-
-                        for (GeometryInterface geom : intersection) {
-                            Rectangle rectangle = (Rectangle) geom;
-                            record.intersections++;
-
-                            if(!record.rectangles.get(rectangle)) {
-                                record.rectangles.put(rectangle, true);
-                                record.intersections++;
-                            }
-                        }
-                    }
+            for (GeometryInterface geo : intersections) {
+                if(this.labels.containsKey((LabelInterface) geo)) {
+                    this.labels.get((LabelInterface)geo).add(label);
                 }
             }
+
+            this.tree.insert(label);
+            this.labels.put(label, new HashSet(intersections));
+
+            this.xMin = Math.min(label.getXMin(), this.xMin);
+            this.yMin = Math.min(label.getXMax(), this.yMin);
+            xMax = Math.max((int)label.getXMax(), xMax);
+            yMax = Math.max(label.getYMax(), yMax);
         }
 
-        record.extent = Math.max(xMax - xMin, yMax - yMin);
+        this.extent = Math.max(xMax - xMin, yMax - yMin);
+    }
 
-        return record;
+    public HashMap<LabelInterface, Set<LabelInterface>> getLabels() {
+        return labels;
+    }
+
+    public int getPointCount() {
+        return pointCount;
+    }
+
+    public double getExtent() {
+        return extent;
+    }
+
+    public double getxMin() {
+        return xMin;
+    }
+
+    public double getyMin() {
+        return yMin;
     }
 }
