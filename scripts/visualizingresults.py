4278ffa30aaf780bb6183abb933abe19515b466b
JurisMajors <j.a.majors@student.tue.nl>
2019-03-29 22:23:59 +0100

    visualization of overlaps, axis names, functional decomposition

--- scripts/visualizingresults.py
+++ scripts/visualizingresults.py
@@ -4,10 +4,12 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--jsonloc',help='location of the json to visualize', required=True)
+parser.add_argument('-i', '--inputloc',help='location of the json to visualize', required=True)
+parser.add_argument('-o', '--outputdir',help='directory of graph outputs', required=False)
 
 args = parser.parse_args()
-file_loc = args.jsonloc
+file_loc = args.inputloc
+output_dir = args.outputdir if args.outputdir else ""
 
 def get_dictionary(json_location):
     json_f = open(json_location)
@@ -16,34 +18,49 @@ def get_dictionary(json_location):
 def apply(func, r, x, objective):
    return [func(list(map(int, r[str(labels)][objective]))) for labels in x]
 
-def visualize_time(r):
-    x = sorted([int(labels) for labels in r.keys()])
-    y = []
-    y.append(apply(statistics.mean, r, x, 'Time'))
-    y.append(apply(statistics.median, r, x, 'Time'))
+def visualize_time(r, filename, x):
+    plt.plot(x, apply(statistics.mean, r, x, 'Time'), label='mean')
+    plt.plot(x, apply(statistics.median, r, x, 'Time'), label='median')
+    plt.legend(loc='upper left')
 
-    for y_val in y:
-        plt.plot(x, y_val)
+    saveplot(filename, "|P|", "t(ms)", "Running time")
 
-    plt.savefig("time.png")
+# saves currently opened plot and closes it
+def saveplot(filename, xlabel, ylabel, title):
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
 
-def visualize_optimality(r):
-    x = sorted([int(labels) for labels in r.keys()])
+    plt.savefig(f"{output_dir}{filename}.png")
+    plt.close()
+
+# currently assumes that optimal height is always 10.0
+def visualize_optimality(r, filename, x):
     optim_dict = {}
     y = []
     for label in x:
         optim_dict[label] = [float(h) / 10.0 for h in r[str(label)]['Height']]
 
-    y.append([statistics.mean(optim_dict[labels]) for labels in x])
-    y.append([statistics.median(optim_dict[labels]) for labels in x])
-    for y_var in y:
-        plt.plot(x, y_var)
-    plt.savefig("optimality.png")
+    plt.plot(x, [statistics.mean(optim_dict[labels]) for labels in x], label='mean')
+    plt.plot(x, [statistics.median(optim_dict[labels]) for labels in x], label='median')
+    plt.legend(loc='upper left')
 
-def visualize():
+    saveplot(filename, "|P|", "h/h_opt", "Optimality")
+
+def visualize_overlaps(r, filename, x):
+    plt.plot(x, apply(sum, r, x, 'Overlaps'), label='total')
+    plt.plot(x, apply(statistics.mean, r, x, 'Overlaps'), label='mean')
+    plt.plot(x, apply(statistics.median, r, x, 'Overlaps'), label='median')
+    plt.legend(loc='upper left')
+
+    saveplot(filename, "|P|", "#overlaps", "Overlaps")
+
+def visualize(algo, difficulty):
     results = get_dictionary(file_loc)
-    # visualize_time(results)
-    visualize_optimality(results)
+    x = sorted([int(labels) for labels in results.keys()])
 
+    visualize_time(results, f"{algo}_time_{difficulty}", x)
+    visualize_optimality(results, f"{algo}_optimality_{difficulty}", x)
+    visualize_overlaps(results, f"{algo}_overlaps_{difficulty}", x)
 
-visualize()
+visualize("2pos", "hard")
