b9a8d662e245adc592e58eb8977e3c2ffcc8266a
Juris Majors <jurismajors@gmail.com>
2019-04-05 11:54:18 +0200

    better names

--- scripts/visualizingresults.py
+++ scripts/visualizingresults.py
@@ -26,8 +26,8 @@ def apply(func, r, x, objective):
    return [func(list(map(int, r[str(labels)][objective]))) for labels in x]
 
 def visualize_time(r, x, diff):
-    plt.plot(x, apply(statistics.mean, r, x, 'Time'), label=diff + "_" + 'mean')
-    plt.plot(x, apply(statistics.median, r, x, 'Time'), label=diff + "_" + 'median')
+    plt.plot(x, apply(statistics.mean, r, x, 'Time'), label='mean of ' + diff)
+    plt.plot(x, apply(statistics.median, r, x, 'Time'), label='median of ' + diff )
     plt.legend(loc='upper left')
 
 def axis_and_title(xlabel, ylabel, title):
@@ -47,16 +47,20 @@ def visualize_optimality(r, x, diff):
     for label in x:
         optim_dict[label] = [float(h) / 10.0 for h in r[str(label)]['Height']]
 
-    plt.plot(x, [statistics.mean(optim_dict[labels]) for labels in x], label=diff + "_" + 'mean')
-    plt.plot(x, [statistics.median(optim_dict[labels]) for labels in x], label=diff + "_" + 'median')
+    plt.plot(x, [statistics.mean(optim_dict[labels]) for labels in x], label='mean of ' + diff )
+    plt.plot(x, [statistics.median(optim_dict[labels]) for labels in x], label='median of ' + diff)
     plt.legend(loc='upper left')
 
 
-def visualize_overlaps(r, x, diff):
-    plt.plot(x, apply(sum, r, x, 'Overlaps'), label='total')
-    plt.plot(x, apply(statistics.mean, r, x, 'Overlaps'), label=diff + '_' + 'mean')
-    plt.plot(x, apply(statistics.median, r, x, 'Overlaps'), label=diff + '_' + 'median')
-    plt.legend(loc='upper left')
+def visualize_overlaps(r, x, diff, hist=False):
+    if hist:
+        plt.hist(apply(sum, r, x, 'Overlaps'), bins=len(x))
+    else:
+        plt.plot(x, apply(sum, r, x, 'Overlaps'), label=diff + '_' + 'total')
+        plt.plot(x, apply(statistics.mean, r, x, 'Overlaps'), label='mean of ' + diff)
+        plt.plot(x, apply(statistics.median, r, x, 'Overlaps'), label='median of ' + diff)
+
+        plt.legend(loc='upper left')
 
 
 def visualize(stamp):
@@ -64,21 +68,22 @@ def visualize(stamp):
     x = sorted([int(labels) for labels in hard.keys()])
     x_label = "|P|"
 
-    plt.figure(figsize=(20, 5))
-    plt.subplot(1, 3, 1)
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
     visualize_time(hard, x, "hard")
+    plt.subplot(1, 2, 2)
     visualize_time(easy, x, "easy")
     axis_and_title(x_label, "time(ms)", "Running time")
 
-    plt.subplot(1, 3, 2)
-    visualize_optimality(hard, x, "hard")
-    visualize_optimality(easy, x, "easy")
-    axis_and_title(x_label, "h/h_opt", "Optimality")
+    # plt.subplot(1, 3, 2)
+    # visualize_optimality(hard, x, "hard")
+    # visualize_optimality(easy, x, "easy")
+    # axis_and_title(x_label, "h/h_opt", "Optimality")
 
-    plt.subplot(1, 3, 3)
-    visualize_overlaps(hard, x, "hard")
-    visualize_overlaps(easy, x, "easy")
-    axis_and_title(x_label, "#overlaps", "Overlaps")
+    # plt.subplot(1, 3, 3)
+    # # visualize_overlaps(hard, x, "hard")
+    # visualize_overlaps(easy, x, "easy")
+    # axis_and_title(x_label, "#overlaps", "Overlaps")
     saveplot(str(hard_name).partition('.')[0] + "_" + stamp)
 
 visualize(time.strftime("%Y%m%d-%H%M%S"))
