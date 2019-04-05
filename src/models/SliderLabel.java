c3f9b2211d8fafc88c656580abd19e9d54fee944
Chess <34745596+Chessnl@users.noreply.github.com>
2019-04-05 11:16:49 +0200

    Sven is such a dipshit to not be able to work with rectangles

--- src/models/SliderLabel.java
+++ src/models/SliderLabel.java
@@ -35,25 +35,6 @@ public class SliderLabel extends AbstractLabel {
     }
 
     /**
-     * Parses arguments
-     *
-     * @param x the x-coordinate of the POI
-     * @param y the y-coordinate of the POI
-     * @param height the height of the rectangle
-     * @param aspectRatio the aspect ratio of the rectangle
-     * @param shift the shift value of the rectangle with respect to the POI
-     * @return the {@link Rectangle} defined by given parameters
-     */
-    public static Rectangle parseRectangle (double x, double y, double height, double aspectRatio, double shift) {
-        if (x < 0 || x > 10000 || y < 0 || y > 10000) throw new IllegalArgumentException("SliderLabel.parseRectangle has POI coordinates not in [0, 10000] x [0, 10000]");
-        if (height <= 0) throw new IllegalArgumentException("SliderLabel.parseRectangle does not allow height <= 0");
-        if (aspectRatio <= 0) throw new IllegalArgumentException("SliderLabel.parseRectangle does not allow aspectRatio <= 0");
-        if (shift < 0 || shift > 1) throw new IllegalArgumentException("SliderLabel.parseRectangle requires a shift being [0, 1]");
-
-        return new Rectangle(x + (shift - 1) * height * aspectRatio, y, x + shift * height * aspectRatio, y + height);
-    }
-
-    /**
      * @return this labels sequence starting x-coordinate
      */
     public int getSeqStartX() {
